113a824d4342ff7d5001c910493021bcd18245b1
김지환 <jihwan-morning@mz.co.kr>
2023-08-09 13:40:52 +0900

    change signup page

---
+++ app/src/main/java/com/example/mychat/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mychat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mychat.databinding.ActivityLoginBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
